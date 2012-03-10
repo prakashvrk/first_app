df51a5aa338f5ba7516ab5fb97d680d70c49d9e8
U-NOAM\Pvarana <Pvarana@dblnbk75276.noam.corp.frk.com>
2012-03-10 09:10:43 -0800

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3c9d6f4c5258f523a889f5cfc68ec51e3a445fecaf2e3b82bec57cc84f2f952df1ade8935a60f86a5c758ae185458af95eeca15cdce5d91f5311156049391dd9'
